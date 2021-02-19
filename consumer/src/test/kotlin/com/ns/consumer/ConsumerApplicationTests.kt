e45f99c44b7b9947289495356e86d523f5d089b7
niraj <niraj.sonawane@gmail.com>
2021-02-19 09:15:27 +0100

    removed un used imports

--- consumer/src/test/kotlin/com/ns/consumer/ConsumerApplicationTests.kt
+++ consumer/src/test/kotlin/com/ns/consumer/ConsumerApplicationTests.kt
@@ -1,12 +1,7 @@
 package com.ns.consumer
 
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.RegisterExtension
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.cloud.contract.stubrunner.junit.StubRunnerExtension
-import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties
 
 @SpringBootTest
 class ConsumerApplicationTests {
